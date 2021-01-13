bfbcc43a76ea897ce906f3ede6fba66dcc4d41ea
Gabriel Gebril <gebril.gabriel@gmail.com>
2021-01-13 17:51:29 -0500

    Added drilldown for conference widget. Added events list. Added Conference registration. Added sponsoring a conference. Added reminders.

--- website/src/components/browser/BrowserHome/index.js
+++ website/src/components/browser/BrowserHome/index.js
@@ -1,35 +1,20 @@
 import React from "react";
 import { Container, Grid, Typography } from "@material-ui/core";
 import { Sidebar } from "@browser/FilterSidebar";
-import { NumberWidget } from "@admin/common/NumberWidget";
-import { EventWidget } from "@browser/EventWidget";
+import { ConferenceWidget } from "@browser/ConferenceWidget";
 import "./dashboard.css";
 
 /**
- * Component for the /admin landing page
- * See README for figma links to mockup design
+ * Component for the /browser
  * @param {*} props
  */
-export const BrowserHome = (props) => {
 
-  const applicationsData = {
-    labels: ['Pending', 'Accepted'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [156, 680],
-        backgroundColor: [
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)'
-        ],
-        borderWidth: 1,
-      },
-    ],
-  }
+
+
+
+
+export const BrowserHome = (props) => {
+  const [open, setOpen] = React.useState(false);
 
   return (
     <Container className="dashboard" maxWidth="lg">
@@ -47,18 +32,19 @@ export const BrowserHome = (props) => {
             Conferences
           </Typography>
           <Grid container spacing={2}>
-            {/* Application/Event Traffic overview */}
+            {/* Collection of conferences*/}
             <Grid className="application-metric" item xs={4}>
-              <EventWidget date="Thu, Jan 28, 2021 6:30 PM EST" image="money.jpg" title="Finance Workshop" />
+              <ConferenceWidget date="Thu, Jan 28, 2021 6:30 PM EST" image="money.jpg" title="Finance Workshop" desc={"Learn about money."} events={["Learn","Eat"]}/>
             </Grid>
-            <Grid className="application-metric" item xs={4}>
-            <EventWidget date="Thu, Jan 28, 2021 6:30 PM EST" image="money.jpg" title="Finance Workshop" />
 
-            </Grid>
             <Grid className="application-metric" item xs={4}>
-            <EventWidget date="Thu, Jan 28, 2021 6:30 PM EST" image="money.jpg" title="Finance Workshop" />
-
+            <ConferenceWidget date="Thu, Jan 28, 2021 6:30 PM EST" image="supersmash.png" title="Smash Tournament" desc={"Compete in a local Smash torunament."} events={["Opening Ceremonies","Eat"]}/>
             </Grid>
+
+            <Grid className="application-metric" item xs={4}>
+            <ConferenceWidget date="Thu, Jan 28, 2021 6:30 PM EST" image="hack.png" title="Hackathon" desc={"Make apps and compete for prizes."} events={["Hack","Eat", "Hack"]}/>
+            </Grid>
+            
           </Grid>
         </Grid>
       </Grid>
