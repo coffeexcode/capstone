93698998dc49f4cd558dbca04750c4233549fbb5
Ethan Johnston <johnse20@mcmaster.ca>
2021-01-13 16:12:50 -0500

    Add product and company detail pages to website (#13)
    * Add product offering page
    * Add "about us" section to the public facing website
    * Update CHANGELOG formatting for readability

--- website/src/components/Home/index.js
+++ website/src/components/Home/index.js
@@ -1,23 +1,44 @@
 import React from "react";
 import { Button, Container, Grid } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 import "./home.css";
 
 export const Home = (props) => {
-
+  const history = useHistory();
+  const redirect = (endpoint) => history.push(endpoint);
   return (
-    <Container className="admin-home" maxWidth="lg">
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <div className="landing-call">
-            <p style={{fontSize: "24px"}}>One Platform, Any Event</p>
-            <p style={{fontSize: "36px"}}>Non-profit event organizing at scale</p>
-            <Button variant="contained" color="primary">Get Started</Button>
-          </div>
+    <div>
+      <Container className="admin-home" maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid className="home-row" item xs={6}>
+            <div className="landing-call">
+              <p style={{ fontSize: "24px" }}>One Platform, Any Event</p>
+              <p style={{ fontSize: "36px" }}>
+                Non-profit event organizing at scale
+              </p>
+              <Button variant="contained" color="primary">
+                Get Started
+              </Button>
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="event-img" src="./img/undraw_event.svg" />
+          </Grid>
+          <Grid className="browse-row" item xs={6}>
+            <img className="browse-img" src="./img/browse.svg" />
+          </Grid>
+          <Grid className="browse-row" item xs={6}>
+            <div className="landing-call">
+              <p style={{ fontSize: "24px" }}>Find an event today</p>
+              <p style={{ fontSize: "36px" }}>Browse events in your area</p>
+              <Button onClick={() => redirect("/browse")} variant="contained" color="primary">
+                Browse
+              </Button>
+            </div>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <img className="event-img" src="./undraw_event.svg" />
-        </Grid>
-      </Grid>
-    </Container>
-  )
-}
+      </Container>
+      <div className="bg-color"></div>
+    </div>
+  );
+};
