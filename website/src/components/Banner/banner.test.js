853e54c66e4a5bf3b95af53bacead8620cd6a1c1
Ethan Johnston <johnse20@mcmaster.ca>
2021-02-06 19:54:03 -0500

    uncomment some of the older failing unit tests

--- website/src/components/Banner/banner.test.js
+++ website/src/components/Banner/banner.test.js
@@ -19,17 +19,17 @@ describe( '<Banner/>', () => {
   });
   
   
-  // it('downloads link goes to correct path*', () => {
-  //   renderWithRouter(<App />);
-  //   const linkElement = screen.getByTestId("downloads");
-  //   fireEvent.click(linkElement, { button: 0 });
-  //   expect(screen.getByTestId(/downloads-page/i)).toBeInTheDocument();
-  // })
+  it('goes to the correct component on clicking the downloads link', () => {
+    renderWithRouter(<App />);
+    const linkElement = screen.getByTestId("downloads");
+    fireEvent.click(linkElement, { button: 0 });
+    expect(screen.getByTestId(/downloads-page/i)).toBeInTheDocument();
+  })
   
-  // it('account link goes to correct path*', () => {
-  //   renderWithRouter(<App />);
-  //   const linkElement = screen.getByTestId("account");
-  //   fireEvent.click(linkElement, { button: 0 });
-  //   expect(screen.getByTestId(/account-page/i)).toBeInTheDocument();
-  // })
+  it('goes to the correct component on clicking the account link', () => {
+    renderWithRouter(<App />);
+    const linkElement = screen.getByTestId("account");
+    fireEvent.click(linkElement, { button: 0 });
+    expect(screen.getByTestId(/account-page/i)).toBeInTheDocument();
+  })
 })
--- website/src/components/admin/common/__tests__/Sidebar.test.js
+++ website/src/components/admin/common/__tests__/Sidebar.test.js
@@ -1,25 +1,30 @@
-import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent } from "@testing-library/react";
 import { renderWithRouter } from "../../../../utils/test";
-import App from '../../../../App';
+import App from "../../../../App";
+import { IsoTwoTone } from "@material-ui/icons";
 
-test('sidebar statistics link goes to correct path', () => {
-  renderWithRouter(<App />, { route: "/admin"});
-  const linkElement = screen.getByTestId("statistics");
-  fireEvent.click(linkElement, { button: 0 });
-  expect(screen.getByTestId(/statistics-page/i)).toBeInTheDocument();
+describe("<Sidebar />", () => {
+  it("should render statistics component on clicking link", () => {
+    renderWithRouter(<App />, { route: "/admin" });
+
+    const linkElement = screen.getByTestId("statistics");
+    fireEvent.click(linkElement, { button: 0 });
+    expect(screen.getByTestId(/statistics-page/i)).toBeInTheDocument();
+  });
+
+  it("should render registrations component on clicking link", () => {
+    renderWithRouter(<App />, { route: "/admin" });
+
+    const linkElement = screen.getByTestId("registrations");
+    fireEvent.click(linkElement, { button: 0 });
+    expect(screen.getByTestId(/registrations-page/i)).toBeInTheDocument();
+  });
+
+  it("should render admin help component on clicking link", () => {
+    renderWithRouter(<App />, { route: "/admin"});
+
+    const linkElement = screen.getByTestId("help-admin");
+    fireEvent.click(linkElement, { button: 0 });
+    expect(screen.getByTestId(/help-admin-page/i)).toBeInTheDocument();
+  })
 });
-
-test('sidebar registrations link goes to correct path', () => {
-  renderWithRouter(<App />, { route: "/admin"});
-  const linkElement = screen.getByTestId("registrations");
-  fireEvent.click(linkElement, { button: 0 });
-  expect(screen.getByTestId(/registrations-page/i)).toBeInTheDocument();
-});
-
-
-// test('sidebar registrations link goes to correct path', () => {
-//   renderWithRouter(<App />, { route: "/admin"});
-//   const linkElement = screen.getByTestId("help-admin");
-//   fireEvent.click(linkElement, { button: 0 });
-//   expect(screen.getByTestId(/help-admin-page/i)).toBeInTheDocument();
-// });
