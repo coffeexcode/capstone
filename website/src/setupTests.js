02b6b35277b9e56d5fc53a9517b3d80bd67113c5
Ethan Johnston <johnse20@mcmaster.ca>
2021-02-07 21:54:13 -0500

    remove canvas package from dependencies

--- website/src/setupTests.js
+++ website/src/setupTests.js
@@ -4,6 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
+import 'jest-canvas-mock';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
