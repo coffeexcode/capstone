b7d9de5480653d8e65c1b878ad187128b50231db
Ethan Johnston <johnse20@mcmaster.ca>
2021-03-30 14:03:08 -0400

    Update applicant list to properly interact with api (#90)
    * Allow for actions against a user in the table that propagate to the API correctly
    * Include a loading spinner while it fetches data from the api and loads it
    * Update instructions on running the frontend locally now that docker is required

--- website/src/components/admin/Applicants/Details.jsx
+++ website/src/components/admin/Applicants/Details.jsx
@@ -1,29 +1,126 @@
-import { Grid, Card, CardContent, Typography } from "@material-ui/core";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  CardActions,
+  Button,
+} from "@material-ui/core";
 import { Document, Page } from "react-pdf";
-import PDFViewer from 'pdf-viewer-reactjs';
-import React from "react";
+import { v1 } from "@utils/data";
+import PDFViewer from "pdf-viewer-reactjs";
+import React, { useState } from "react";
 
+/**
+ * Drop-down panel for displaying additional information about an applicant
+ * as well as acting on that information (i.e. accept / reject).
+ * @param {fn} props.update method to propogate changes made to the list of
+ * attendees to the state in components above.
+ * @param {Object} props.data the data of the row these details belong to
+ */
 export const Details = (props) => {
+  const [showPDF, setShowPDF] = useState(true);
+
+  const lorem =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const acceptApplicant = async () => {
+    const updatedData = { ...props.data, status: "Accepted" };
+    const res = await v1.updateAttendee(updatedData);
+    // PDF canvas must be teared down prior to re-render
+    // they do not expose it directly, thus toggle it
+    // TODO: when filtering by pending application, it get's filtered out
+    // on accept/reject
+    setShowPDF(false);
+    props.update(updatedData);
+    setShowPDF(true);
+  };
+
+  const rejectApplicant = async () => {
+    const updatedData = { ...props.data, status: "Rejected" };
+    const res = await v1.updateAttendee(updatedData);
+    // see above explaination
+    setShowPDF(false);
+    props.update(updatedData);
+    setShowPDF(true);
+  };
 
   return (
     <div className="table-details">
       <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <PDFViewer document={{url: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Resume.pdf"}} />
+        <Grid item xs={8}>
+          {showPDF ? (
+            <PDFViewer
+              hideNavbar
+              document={{
+                url:
+                  "https://upload.wikimedia.org/wikipedia/commons/c/cc/Resume.pdf",
+              }}
+            />
+          ) : (
+            <div></div>
+          )}
         </Grid>
-        <Grid item xs={6}>
-          <Card variant="outlined">
+        <Grid item xs={4}>
+          <Card variant="outlined" className="details-card">
             <CardContent>
               <Typography variant="h5" component="h2">
                 {props.data.name}
               </Typography>
-              <Typography variant="h5" component="h2">
-                TODO
-              </Typography>
+              <Typography color="textSecondary">{props.data.status}</Typography>
+              <br />
+              <TextField
+                fullWidth
+                multiline
+                disabled
+                variant="outlined"
+                label="Question 1"
+                defaultValue={lorem}
+              />
+              <br />
+              <br />
+              <TextField
+                fullWidth
+                multiline
+                disabled
+                variant="outlined"
+                label="Question 2"
+                defaultValue={lorem}
+              />
+              <br />
+              <br />
+              <TextField
+                fullWidth
+                multiline
+                disabled
+                variant="outlined"
+                label="Question 3"
+                defaultValue={lorem}
+              />
+              <br />
+              <br />
             </CardContent>
+            <CardActions>
+              <Button
+                disabled={props.data.status === "Confirmed"}
+                variant="contained"
+                color="primary"
+                onClick={acceptApplicant}
+              >
+                Accept
+              </Button>
+              <Button
+                disabled={props.data.status === "Confirmed"}
+                variant="contained"
+                color="secondary"
+                onClick={rejectApplicant}
+              >
+                Reject
+              </Button>
+            </CardActions>
           </Card>
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
--- website/src/components/admin/Applicants/index.js
+++ website/src/components/admin/Applicants/index.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState, forwardRef } from "react";
 import { Container, Grid, Typography } from "@material-ui/core";
-import {  v1, exportData, acceptApplications } from "@utils/data";
+import { v1, exportData } from "@utils/data";
 import { Sidebar } from "@admin/common/Sidebar";
+import Loader from "react-loader-spinner";
 import { Table } from "./Table";
 import "./applicants.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 /**
- * Component for the /admin/registrations page
+ * Component for the /admin/registrations page that lists
+ * all applicants for your event, with the ability to review
+ * and act on these applicants through a drop down panel
+ * 
  * See README for figma links to mockup design
- * @param {*} props
  */
 export const Registrations = (props) => {
   const [attendees, setAttendees] = useState([]);
@@ -20,23 +24,47 @@ export const Registrations = (props) => {
     setAttendees(rows);
   };
 
+  const update = (data) => {
+    const tmp = [...attendees];
+    setAttendees(tmp.map(v => {
+      if (v.id && v.id === data.id) {
+        return data;
+      }
+      return v;
+    }));
+  }
 
   useEffect(() => {
     getData();
   }, []);
 
   return (
-    <Container data-testid="registrations-page" className="applicants" maxWidth="lg">
+    <Container
+      data-testid="registrations-page"
+      className="applicants"
+      maxWidth="lg"
+    >
       <Grid container spacing={5}>
         <Grid item xs={3}>
           <Sidebar />
         </Grid>
         <Grid item xs={9}>
-          <Table
-            accept={acceptApplications}
-            export={exportData}
-            data={attendees}
-          />
+          {attendees.length > 0 ? (
+            <Table
+              export={exportData}
+              data={attendees}
+              update={update}
+            />
+          ) : (
+            <Loader
+              style={{marginTop: "20%"}}
+              type="Puff"
+              color="#6b9cd5"
+              height={100}
+              width={100}
+              timeout={3000} //3 secs
+            />
+          )}
         </Grid>
       </Grid>
     </Container>
